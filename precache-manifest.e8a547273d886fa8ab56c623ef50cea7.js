f1f4cbc313d62e006c8c97461bbac0974cdc4cea
Hannah Abraham <hsabraham28@berkeley.edu>
2020-07-17 02:23:03 -0500

    Updates

---
+++ precache-manifest.e8a547273d886fa8ab56c623ef50cea7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5621a65781ea27e17bd90b01f3540f93",
+    "url": "/bear-homepage/index.html"
+  },
+  {
+    "revision": "7bf4bbe99d5b71588a57",
+    "url": "/bear-homepage/static/css/main.748c3dab.chunk.css"
+  },
+  {
+    "revision": "a1c811ee40af24b0d8ae",
+    "url": "/bear-homepage/static/js/2.c6ab0aee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bear-homepage/static/js/2.c6ab0aee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bf4bbe99d5b71588a57",
+    "url": "/bear-homepage/static/js/main.710a44ac.chunk.js"
+  },
+  {
+    "revision": "b5bba0abc63ca78e0ebb",
+    "url": "/bear-homepage/static/js/runtime-main.af2734dc.js"
+  },
+  {
+    "revision": "93e1d0363d67717dc4ceed7e8531ccad",
+    "url": "/bear-homepage/static/media/cal-stadium.93e1d036.jpg"
+  },
+  {
+    "revision": "bbc714b60742e30e5839d22333327181",
+    "url": "/bear-homepage/static/media/connect-icon.bbc714b6.PNG"
+  },
+  {
+    "revision": "3632d881c74ee4f47acd5fce10fe5507",
+    "url": "/bear-homepage/static/media/our-story-icon.3632d881.PNG"
+  },
+  {
+    "revision": "e2b231a6707c44e6f20dc956ee97572c",
+    "url": "/bear-homepage/static/media/virtual-sproul-icon.e2b231a6.PNG"
+  }
+]);
